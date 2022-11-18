9abf77519070c176819f5e6668a54944daad5aaa
jackwotherspoon <jackwoth@google.com>
2022-11-18 15:41:51 +0000

    chore: load cert chain

--- tests/conftest.py
+++ tests/conftest.py
@@ -265,9 +265,11 @@ def mysql_server(mysql_address):
         ssl_directory = os.path.join(os.path.dirname(__file__),
                                      'ssl_resources', 'ssl')
         ca_file = os.path.join(ssl_directory, 'ca.pem')
-
+        cert_file = os.path.join(ssl_directory, 'server-cert.pem' )
+        key_file = os.path.join(ssl_directory, 'server-key.pem')
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         ctx.check_hostname = False
+        ctx.load_cert_chain(cert_file, keyfile=key_file)
         ctx.load_verify_locations(cafile=ca_file)
         # ctx.verify_mode = ssl.CERT_NONE
 
